01bdc8af0329724d892cd858e69c1f3d50774759
Andrew Seales <andrew.seales@ed.ac.uk>
2017-03-03 12:11:44 +0000

    Resolves #2 - proj4 not working:
      * Quick and dirty fix to get proj4 working. For some reason the module
        is imported into a `defaults` object inside `proj4`. I had to remove
        the `@types/proj4` typescript definition to make it let me do this.
        Need to sort this properly for the real app.

--- src/app/map/map.service.ts
+++ src/app/map/map.service.ts
@@ -20,10 +20,10 @@ import { ConfigService } from '../config/config.service';
 
 // import { Map as OlMap } from 'openlayers';
 
-proj4.defs('EPSG:27700', '+proj=tmerc +lat_0=49 +lon_0=-2 +k=0.9996012717' +
+proj4.default.defs('EPSG:27700', '+proj=tmerc +lat_0=49 +lon_0=-2 +k=0.9996012717' +
                          ' +x_0=400000 +y_0=-100000 +ellps=airy +towgs84=446.448,' +
                          '-125.157,542.06,0.15,0.247,0.842,-20.489 +units=m +no_defs');
-proj.setProj4(proj4);
+proj.setProj4(proj4.default);
 
 @Injectable()
 export class MapService implements OnInit {
