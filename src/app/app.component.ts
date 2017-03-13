bd3e98ec3cbe627fc94e1a8e43bd1ad9f267c762
Andrew Seales <andrew.seales@ed.ac.uk>
2017-03-13 13:41:40 +0000

    Add per client customisation.
    Only geology is customised right now, and the support is relatively
    basic.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,26 @@
-import { Component } from '@angular/core';
+import { Location } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  theme: string;
+
+  constructor(private location: Location) { }
+  ngOnInit() {
+    // Can't use the ActivatedRoute interface here because we're getting the route outside the router-outlet
+    let path = this.location.path(false);
+    // Extract the last part of the URL path, while filtering off any query parameters.
+    // The path is restricted to lower case a-z because all our collections fit into that pattern.
+    let collection = path.match(/.*\/([a-z]+)(\?.*)?/);
+
+    if (!collection) {
+      throw `invalid url - ${path}`;
+    }
+
+    this.theme = `${collection[1]}-theme`;
+  }
 }
