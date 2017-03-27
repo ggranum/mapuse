58684c4dd1c6ae49daf7aabfa8970614a81489c9
Andrew Seales <andrew.seales@ed.ac.uk>
2017-03-27 16:04:12 +0100

    Upgrade to Angular 4 and angular-cli 1 now that they are final.
    Had to turn off strictNullChecks for angular 4 :(
    Fix will be in 4.1.

--- src/app/menu/components/common/annotations/draw-line/draw-line.service.spec.ts
+++ src/app/menu/components/common/annotations/draw-line/draw-line.service.spec.ts
@@ -1,6 +1,6 @@
 /* tslint:disable:no-unused-variable */
 
-import { TestBed, async, inject } from '@angular/core/testing';
+import { TestBed, inject } from '@angular/core/testing';
 import { DrawLineService } from './draw-line.service';
 
 describe('Service: DrawLine', () => {
