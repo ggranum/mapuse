4685394d82b651b09a29a79f2705fdf1898213f1
Andrew Seales <andrew.seales@ed.ac.uk>
2017-04-05 12:05:33 +0100

    Fixes #11 - move layout definition to inside client module:
      * This allows clients to more easily change which layout they're
        using. Further improvement is probably possible when we have a real
        use case.

--- src/app/client/client.module.ts
+++ src/app/client/client.module.ts
@@ -1,20 +1,22 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { MaterialModule } from '@angular/material';
+import { CovalentCoreModule } from '@covalent/core';
+import { LayoutModule } from '../layout/layout.module';
+import { MapModule } from '../map/map.module';
+import { MenuModule } from '../menu/menu.module';
+import { SearchModule } from '../tools/search/search.module';
 import { ClientComponent } from './client.component';
-import { MapModule } from "../map/map.module";
-import { SearchModule } from "../tools/search/search.module";
-import { MenuModule } from "../menu/menu.module";
-import { MaterialModule } from "@angular/material";
-import { CovalentCoreModule } from "@covalent/core";
 
 @NgModule({
   imports: [
     CommonModule,
-    MapModule,
-    SearchModule,
-    MenuModule,
-    MaterialModule,
     CovalentCoreModule,
+    LayoutModule,
+    MapModule,
+    MaterialModule,
+    MenuModule,
+    SearchModule,
   ],
   declarations: [ClientComponent],
   exports: [ClientComponent],
