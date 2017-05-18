5606e7669d5fb2da29aac3f62c49f79f920fac92
Andrew Seales <andrew.seales@ed.ac.uk>
2017-05-18 09:20:53 +0100

    Tidy ups in both basemap tool modules.

--- src/app/tools/basemaps/basemaps.module.ts
+++ src/app/tools/basemaps/basemaps.module.ts
@@ -3,16 +3,12 @@ import { NgModule } from '@angular/core';
 import { MaterialModule } from '@angular/material';
 import { BasemapsButtonComponent } from './basemaps-button.component';
 
-import { MapModule } from '../../map/map.module';
-
 @NgModule({
   imports: [
     CommonModule,
-    MapModule,
     MaterialModule,
   ],
   declarations: [BasemapsButtonComponent],
-  providers: [],
   exports: [BasemapsButtonComponent],
 })
 export class BasemapsModule { }
