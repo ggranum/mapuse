870747fdda4a4c66026a93cb70f2738d6664a988
Andrew Seales <andrew.seales@ed.ac.uk>
2017-05-30 09:26:56 +0100

    Basemaps now uses an array rather than hard coded HTML.

--- src/app/tools/basemaps/basemaps-button.component.ts
+++ src/app/tools/basemaps/basemaps-button.component.ts
@@ -9,7 +9,39 @@ export class BasemapsButtonComponent implements OnInit {
 
   panelVisible = false;
 
-  constructor() { }
+  overlays: [{name: string, value: string, icon: string}];
+
+  constructor() {
+    this.overlays = [{
+      name: 'MasterMap',
+      value: 'mastermap',
+      icon: 'assets/thumbnails/MasterMap.png',
+    }, {
+      name: 'MasterMap Plan',
+      value: 'mastermapplan',
+      icon: 'assets/thumbnails/MasterMapOrig.png',
+    }, {
+      name: 'OML',
+      value: 'oml',
+      icon: 'assets/thumbnails/OML.png',
+    }, {
+      name: 'Raster 25k',
+      value: 'raster25k',
+      icon: 'assets/thumbnails/Raster25k.png',
+    }, {
+      name: 'GB',
+      value: 'gb',
+      icon: 'assets/thumbnails/gb.png',
+    }, {
+      name: 'VML Plan',
+      value: 'vmlplan',
+      icon: 'assets/thumbnails/VMLPlan.png',
+    }, {
+      name: 'VMD',
+      value: 'vmd',
+      icon: 'assets/thumbnails/VMD.png',
+    }];
+  }
 
   togglePanel() {
     this.panelVisible = !this.panelVisible;
