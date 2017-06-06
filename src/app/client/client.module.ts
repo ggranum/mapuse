b1adc06a14612d02e403bdc60797dc56542d279a
Andrew Seales <andrew.seales@ed.ac.uk>
2017-06-06 15:09:58 +0100

    Oops forgot to remove the google style basemap from the module definition.

--- src/app/client/client.module.ts
+++ src/app/client/client.module.ts
@@ -5,7 +5,6 @@ import { MaterialModule } from '@angular/material';
 import { LayoutModule } from '../layout/layout.module';
 import { MapModule } from '../map/map.module';
 import { SidebarModule } from '../sidebar/sidebar.module';
-import { BasemapsModule as BasemapsGoogleModule } from '../tools/basemaps-google/basemaps.module';
 import { ToolsModule } from '../tools/tools.module';
 import { ClientComponent } from './client.component';
 
@@ -16,7 +15,6 @@ import { ClientComponent } from './client.component';
     MapModule,
     MaterialModule,
     SidebarModule,
-    BasemapsGoogleModule,
     ToolsModule,
   ],
   declarations: [ClientComponent],
