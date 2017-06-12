b4584735d2a30edd6d9707e97c8cb9c703b08d62
Nicolo Zanetti <nicolo.zanetti@gmail.com>
2017-06-12 15:42:30 +0100

    Missing imports for simple-notification's test fix.

--- src/app/tools/search/search.component.spec.ts
+++ src/app/tools/search/search.component.spec.ts
@@ -1,7 +1,14 @@
 /* tslint:disable:no-unused-variable */
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { SearchComponent } from './search.component';
+import {CommonModule} from '@angular/common';
+import {ReactiveFormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
+import {MaterialModule} from '@angular/material';
+import {SimpleNotificationsModule} from 'angular2-notifications/dist';
+import {MapModule} from '../../map/map.module';
+import {SearchComponent} from './search.component';
+import {SearchService} from './search.service';
 
 describe('SearchComponent', () => {
   let component: SearchComponent;
