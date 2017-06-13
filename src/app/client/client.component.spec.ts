b8ec09bfa5449c884edbe7da79520bf33b9a4850
ggranum <geoff.granum@gmail.com>
2017-06-13 16:56:24 +0100

    build(tests): Add Mock for MapConfigService, fix remaining tests
    Adds a dumb mock that returns the same value as is present in the
    Proxy file 'mapconfig.js' for the OS config. Avoids modifying
    any existing code until naming for the upcoming abstract classes
    is nailed down.

--- src/app/client/client.component.spec.ts
+++ src/app/client/client.component.spec.ts
@@ -1,21 +1,27 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { ClientComponent } from './client.component';
-
-import { APP_BASE_HREF, CommonModule } from '@angular/common';
+import {
+  APP_BASE_HREF,
+  CommonModule
+} from '@angular/common';
+import {
+  async,
+  ComponentFixture,
+  TestBed
+} from '@angular/core/testing';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { MaterialModule } from '@angular/material';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { SimpleNotificationsModule } from 'angular2-notifications/dist';
-import { EventManagerService } from '../events/event-manager.service';
-import { MapModule } from '../map/map.module';
-import { LayoutModule } from '../layout/layout.module';
-import { ToolsModule } from '../tools/tools.module';
-import { SidebarModule } from '../sidebar/sidebar.module';
-import { MapService } from '../map/map.service';
-import { MapConfigService } from '../config/map-config.service';
 import { configServiceFactory } from '../config/config.service';
+import { MapConfigService } from '../config/map-config.service';
+import { MockOsMapConfigService } from '../config/mock-os-map-config.service.spec';
+import { EventManagerService } from '../events/event-manager.service';
+import { LayoutModule } from '../layout/layout.module';
+import { MapModule } from '../map/map.module';
+import { MapService } from '../map/map.service';
+import { SidebarModule } from '../sidebar/sidebar.module';
+import { ToolsModule } from '../tools/tools.module';
+import { ClientComponent } from './client.component';
 import { osConfigFactory } from './clients/os.module';
 
 describe('ClientComponent', () => {
@@ -27,13 +33,10 @@ describe('ClientComponent', () => {
       imports: [
         BrowserModule,
         BrowserAnimationsModule,
-
         CommonModule,
         ReactiveFormsModule,
         HttpModule,
-        MapModule,
         MaterialModule,
-        SimpleNotificationsModule.forRoot(),
         LayoutModule,
         ToolsModule,
         MapModule,
@@ -43,7 +46,7 @@ describe('ClientComponent', () => {
         {provide: APP_BASE_HREF, useValue: '/'},
         EventManagerService,
         MapService,
-        MapConfigService,
+        {provide: MapConfigService, useClass: MockOsMapConfigService},
         configServiceFactory(osConfigFactory),
       ],
       declarations: [ClientComponent],
--- src/app/config/config.service.spec.ts
+++ src/app/config/config.service.spec.ts
@@ -5,13 +5,17 @@ import {
   TestBed
 } from '@angular/core/testing';
 
-import { ConfigService } from './config.service';
+import {
+  ConfigService,
+  configServiceFactory
+} from './config.service';
 import { ClientConfig } from './map';
+import { osConfigFactory } from '../client/clients/os.module';
 
 describe('Service: Config', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [ConfigService],
+      providers: [configServiceFactory(osConfigFactory)],
     });
   });
 
