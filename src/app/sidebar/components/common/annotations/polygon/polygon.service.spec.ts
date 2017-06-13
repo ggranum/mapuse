7d263ee285c67fd6a28c1124950d2a3dcc074d48
ggranum <geoff.granum@gmail.com>
2017-06-13 09:39:33 +0100

    build(tests): Fix PolygonService spec

--- src/app/sidebar/components/common/annotations/polygon/polygon.service.spec.ts
+++ src/app/sidebar/components/common/annotations/polygon/polygon.service.spec.ts
@@ -1,16 +1,30 @@
 /* tslint:disable:no-unused-variable */
 
-import { TestBed, inject } from '@angular/core/testing';
+import {
+  inject,
+  TestBed
+} from '@angular/core/testing';
 import { PolygonService } from './polygon.service';
+import { DrawLineService } from '../draw-line/draw-line.service';
+import { MapService } from '../../../../../map/map.service';
+import { MapConfigService } from '../../../../../config/map-config.service';
+import { EventManagerService } from '../../../../../events/event-manager.service';
+import { configServiceFactory } from '../../../../../config/config.service';
+import { osConfigFactory } from '../../../../../client/clients/os.module';
+import { HttpModule } from '@angular/http';
 
 describe('Service: Polygon', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [PolygonService]
+      imports: [
+        HttpModule
+      ],
+      providers: [DrawLineService, PolygonService, MapService, MapConfigService, EventManagerService, configServiceFactory(osConfigFactory),]
     });
   });
 
   it('should ...', inject([PolygonService], (service: PolygonService) => {
     expect(service).toBeTruthy();
+
   }));
 });
