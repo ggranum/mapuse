a69b2c1abeb1909a9dba443d75a37d1245e3fc03
Kunepro <nicolo.zanetti@gmail.com>
2017-06-13 11:56:50 +0100

    build(tests): Fix for MapService

--- src/app/map/map.service.spec.ts
+++ src/app/map/map.service.spec.ts
@@ -4,11 +4,24 @@
 
 import { TestBed, inject } from '@angular/core/testing';
 import { MapService } from './map.service';
+import { MapConfigService } from '../config/map-config.service';
+import { configServiceFactory } from '../config/config.service';
+import { osConfigFactory } from '../client/clients/os.module';
+import { HttpModule } from '@angular/http';
+import { EventManagerService } from '../events/event-manager.service';
 
 describe('Service: Map', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [MapService],
+      imports: [
+        HttpModule,
+      ],
+      providers: [
+        MapService,
+        MapConfigService,
+        configServiceFactory(osConfigFactory),
+        EventManagerService
+      ],
     });
   });
 
